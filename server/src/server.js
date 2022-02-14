3dfa8f6b182b9edba73e61ee7547e45ce8349764
ikadgzl <ikadgzl@gmail.com>
2022-02-14 15:46:42 +0300

    added notfound middleware

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,13 @@
 import express from 'express';
+import { notFound } from './middlewares/notFound.js';
 
 const app = express();
+
+app.get('/', (req, res) => {
+  res.send('hi');
+});
+
+app.use(notFound);
 
 const PORT = 4000 || process.env.PORT;
 app.listen(PORT, () => {
